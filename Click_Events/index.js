5a2438a82e54093c2e709487e9ef1a62769b331f
dhanjiRajput <kidechadhanji@gmail.com>
2024-02-14 22:52:50 +0530

    First JQuery

---
+++ Click_Events/index.js
@@ -0,0 +1,39 @@
+$("#btn1").click(()=>{
+    $("img").attr("src","images2.png");
+})
+
+$("#btn2").dblclick(()=>{
+    $("img").attr("src","images2.png");
+})
+
+$("#btn3").click(()=>{
+    $("img").hide();
+})
+
+$("#btn4").click(()=>{
+    $("img").show();
+})
+
+$("#btn5").click(()=>{
+    $("img").toggle();
+})
+
+$("#btn6").contextmenu(()=>{
+    $("img").attr("src","images2.png");
+})
+
+$("#btn7").mouseenter(()=>{
+    $("img").attr("src","images2.png");
+})
+
+$("#btn8").mouseleave(()=>{
+    $("img").attr("src","images2.png");
+})
+
+$("#btn9").click(()=>{
+    $("img").attr("src","image.jpg");
+})
+
+
+
+
---
+++ Link_Jquery/index.js
@@ -0,0 +1,5 @@
+$("h1").text("Welcome to the JQuery");
+
+$("h1").css({color:"green",fontSize:"150px"});
+$("h1").css("font-family","sans-serif");
+$("h1").css("text-align","center");
